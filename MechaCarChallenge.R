e947242e3cb1ecb4e553d1dbb6891c47bf786b7e
Jake Wellman <wellma41@msu.edu>
2022-01-01 20:24:49 -0500

    deliverable 1 update

---
+++ MechaCarChallenge.R
@@ -0,0 +1,18 @@
+### DELIVERABLE 1
+
+#IMPORTING DPLYR
+library(dplyr)
+
+#importing MecahCar_mpg.csv
+library(tidyverse)
+mecha_mpg <- read.csv(file='MechaCar_mpg.csv',check.names = F, stringsAsFactors = F)
+
+# Perform linear regression using the lm() function.
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=mecha_mpg)
+
+# Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=mecha_mpg))
+
+#
